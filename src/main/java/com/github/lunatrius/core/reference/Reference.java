7d2be5583c53e5239ec2404ef3d764207fdd8ad7
Tim Mayr <tim.mayr@gmx.at>
2024-12-01 19:00:29 +0100

    updated build.gradle and version interpolation

--- src/main/java/com/github/lunatrius/core/reference/Reference.java
+++ src/main/java/com/github/lunatrius/core/reference/Reference.java
@@ -6,8 +6,8 @@ import org.apache.logging.log4j.Logger;
 public class Reference {
 	public static final String MODID = "${modid}";
 	public static final String NAME = "${modname}";
-	public static final String VERSION = "${version}";
-	public static final String FORGE = "${forgeversion}";
+	public static final String VERSION = "${modversion}";
+	public static final String FORGE = "${version_forge}";
 	public static final String MINECRAFT = "${mcversion}";
 
 	public static final Logger logger = LogManager.getLogger(Reference.MODID);
